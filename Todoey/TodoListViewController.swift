743c5091ea69cf8a0754690a8c14b8693931ad58
NazarStf <hworker98@gmail.com>
2023-05-23 17:12:44 +0300

    add tableView

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TodoListViewController.swift
 //  Todoey
 //
 //  Created by Philipp Muellauer on 02/12/2019.
@@ -8,13 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
 }
 
