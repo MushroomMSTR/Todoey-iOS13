95e1502816ae53348b09fa7e501861dd5408dc75
NazarStf <hworker98@gmail.com>
2023-05-23 19:41:57 +0300

    Subclassing a UITableViewController

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+	let itemArray = ["Learn Swift", "Read a book", "Buy new course"]
+	
+	
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,6 +21,37 @@ class TodoListViewController: UITableViewController {
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
+	}
+	
+//	// MARK: - Navigation Controller - Navigation Bar - Bar tint
+//
+//	override func viewWillAppear(_ animated: Bool) {
+//		   super.viewWillAppear(animated)
+//		   navigationController?.navigationBar.prefersLargeTitles = true
+//
+//		   let appearance = UINavigationBarAppearance()
+//		   appearance.backgroundColor = .systemBlue
+//		   appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+//		   appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+//
+//		   navigationController?.navigationBar.tintColor = .white
+//		   navigationController?.navigationBar.standardAppearance = appearance
+//		   navigationController?.navigationBar.compactAppearance = appearance
+//		   navigationController?.navigationBar.scrollEdgeAppearance = appearance
+//   }
+	
+	// MARK: - Table view data source
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		// #warning Incomplete implementation, return the number of rows
+		return itemArray.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+		cell.textLabel?.text = itemArray[indexPath.row]
+		
+		return cell
 	}
 	
 }
